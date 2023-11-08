facda7491cb024b5bfeb03257354bd3c1475a445
rodneymaiato <rod.maiato@gmail.com>
2023-11-07 19:24:29 -0800

    update menu functionality

--- themes/my-theme/assets/js/index.js
+++ themes/my-theme/assets/js/index.js
@@ -1,10 +1,18 @@
-const burger = document.querySelector('#burger')
-const menu = document.querySelector('#menu')
-
-burger.addEventListener('click', ()=> {
-  if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
+// Declare variables
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+const navLinks = document.querySelectorAll(".nav-link");
+// Make menu visible after clicking on the hamburger icon
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
   } else {
-    menu.classList.add('hidden')
+    menu.classList.add("hidden");
   }
+});
+// Make menu hidden after clicking on a menu item
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    menu.classList.add("hidden");
+  });
 });
